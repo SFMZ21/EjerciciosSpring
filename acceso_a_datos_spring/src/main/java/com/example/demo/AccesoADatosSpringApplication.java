dd5be46459a50b4369bd7995c6931d629c7042fa
SFMZ21 <smartinezz1@miumg.edu.gt>
2021-04-02 18:45:52 -0600

    Ejercicios finalizados

---
+++ acceso_a_datos_spring/src/main/java/com/example/demo/AccesoADatosSpringApplication.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import org.apache.geode.cache.client.ClientRegionShortcut;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.gemfire.config.annotation.ClientCacheApplication;
+import org.springframework.data.gemfire.config.annotation.EnableEntityDefinedRegions;
+import org.springframework.data.gemfire.repository.config.EnableGemfireRepositories;
+
+@SpringBootApplication
+@ClientCacheApplication(name = "AccessingGemFireDataRestApplication")
+@EnableEntityDefinedRegions(
+  basePackageClasses = Person.class,
+  clientRegionShortcut = ClientRegionShortcut.LOCAL
+)
+@EnableGemfireRepositories
+@SuppressWarnings("unused")
+public class AccesoADatosSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccesoADatosSpringApplication.class, args);
+	}
+
+}
